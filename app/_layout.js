c9c720565b36637c2cf0fb2f8243c9586795039c
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-10-17 13:24:10 -0300

    remove duplicated app

--- app/_layout.js
+++ app/_layout.js
@@ -1,12 +1,10 @@
 import { Stack } from "expo-router";
 import { AppProvider } from "../src/hooks";
-import App from './index.js';
 
 export default function Layout() {
     return (
     <AppProvider>
         <Stack />;
-        <App/>
     </AppProvider>
  );
 }
